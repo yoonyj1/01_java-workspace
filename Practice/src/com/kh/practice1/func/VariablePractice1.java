f56738efb743aa61c166f4de2852101ecc4d32b3
yoonyj1 <yoonyj2410@naver.com>
2022-10-25 13:52:19 +0900

    1352

--- Practice/src/com/kh/practice1/func/VariablePractice1.java
+++ Practice/src/com/kh/practice1/func/VariablePractice1.java
@@ -12,12 +12,12 @@ public class VariablePractice1 {
 		System.out.print("성별을 입력하세요(남/여) : ");
 		char gender = sc.nextLine().charAt(0);
 		
-		System.out.print("나이를 입력하세요 : ");
+		System.out.print("나이를  입력하세요 : ");
 		int age = sc.nextInt();
 		
 		sc.nextLine();
 		
-		System.out.print("키를  입력하세요 : ");
+		System.out.print("키를 입력하세요 : ");
 		double height = sc.nextDouble();
 		
 		System.out.println("키 " + height + "인 " + age + "살 " + gender + "자 " + name + "님 반갑습니다^^");
