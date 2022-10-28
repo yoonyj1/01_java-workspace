909fba45870a1392e301831cf2b1033ed9f9ef26
yoonyj1 <yoonyj2410@naver.com>
2022-10-28 09:06:06 +0900

    221028_1
    1

---
+++ 03_ControlPractice/src/com/kh/practice/chap02/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.practice.chap02.run;
+
+import com.kh.practice.chap02.loop.LoopPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		LoopPractice lp = new LoopPractice();
+		//lp.practice1();
+		//lp.practice2();
+		lp.practice3();
+	}
+
+}
