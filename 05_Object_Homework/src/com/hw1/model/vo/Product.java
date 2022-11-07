d5c2577ef46595c2eb26326ce88e3ed3bc977db1
yoonyj1 <yoonyj2410@naver.com>
2022-11-07 14:45:31 +0900

    202211071445
    1445

--- 05_Object_Homework/src/com/hw1/model/vo/Product.java
+++ 05_Object_Homework/src/com/hw1/model/vo/Product.java
@@ -7,9 +7,9 @@ public class Product {
 	private String productArea;
 	private int price;
 	private double tax;
-	
+
 	public Product() {}
-	
+
 	public Product(String productId, String productName, String productArea, int price, double tax) {
 		this.productId = productId;
 		this.productName = productName;
@@ -17,47 +17,47 @@ public class Product {
 		this.price = price;
 		this.tax = tax;
 	}
-	
+
 	public String getProductId() {
 		return productId;
 	}
-	
+
 	public void setProductId(String productId) {
 		this.productId = productId;
 	}
-	
+
 	public String getProductName() {
 		return productName;
 	}
-	
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-	
+
 	public String getProductArea() {
 		return productArea;
 	}
-	
+
 	public void setProductArea(String productArea) {
 		this.productArea = productArea;
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
+
 	public double getTax() {
 		return tax;
 	}
-	
+
 	public void setTax(double tax) {
 		this.tax = tax;
 	}
-	
+
 	public String information() {
 		return productId + " " + productName + " " + productArea + " " + price + " " + tax; 
 	}
--- 05_Object_Homework/src/com/hw1/run/ProductTest.java
+++ 05_Object_Homework/src/com/hw1/run/ProductTest.java
@@ -8,25 +8,25 @@ public class ProductTest {
 		Product p1 = new Product("ssgnote9", "갤럭시노트9", "경기도 수원", 960_000, 10.0);
 		Product p2 = new Product("lgxnote5", "LG스마트폰5", "경기도 평택", 780_000, 0.7);
 		Product p3 = new Product("ktsnote3", "KT스마트폰3", "서울시 강남", 250_000, 0.3);
-		
+
 		System.out.println(p1.information());
 		System.out.println(p2.information());
 		System.out.println(p3.information());
-		
+
 		System.out.println("======================================");
-		
+
 		p1.setPrice(1_200_000);
 		p2.setPrice(1_200_000);
 		p3.setPrice(1_200_000);
-		
+
 		p1.setTax(0.05);
 		p2.setTax(0.05);
 		p3.setTax(0.05);
-		
+
 		System.out.println(p1.information());
 		System.out.println(p2.information());
 		System.out.println(p3.information());
-		
+
 		System.out.println("======================================");
 
 		System.out.println("상품명: " + p1.getProductName());
@@ -35,7 +35,7 @@ public class ProductTest {
 		System.out.println("부가세 포함 가격: " + (int)(p2.getPrice() + (p2.getPrice() * p2.getTax()))+ "원");
 		System.out.println("상품명: " + p3.getProductName());
 		System.out.println("부가세 포함 가격: " + (int)(p3.getPrice() + (p3.getPrice() * p3.getTax()))+ "원");
-	
+
 	}
 
 }
