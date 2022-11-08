5459f522f693c5f7c495b1207f393b80b71616d0
yoonyj1 <yoonyj2410@naver.com>
2022-11-08 11:58:54 +0900

    202211081158
    202211081158

--- 06_ObjectArray/src/com/kh/chap02_objectArray/model/Phone.java
+++ 06_ObjectArray/src/com/kh/chap02_objectArray/model/Phone.java
@@ -1,5 +1,58 @@
 package com.kh.chap02_objectArray.model;
 
 public class Phone {
+	// 필드부
+	// 이름, 브랜드명, 가격, 시리즈
+	private String name;
+	private String brand;
+	private int price;
+	private String series;
+	
+	// 생성자부
+	public Phone() {}
+	
+	public Phone(String name, String brand, int price, String series) {
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+		this.series = series;
+	}
 
+	// 메소드부
+	// getter setter
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getSeries() {
+		return series;
+	}
+	
+	public void setSeries(String series) {
+		this.series = series;
+	}
+	
+	public String information() {
+		return name + " " + brand + " " + price + " " + series;
+	}
 }
--- 06_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
+++ 06_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
@@ -1,10 +1,11 @@
 package com.kh.chap02_objectArray.run;
 
+import com.kh.chap02_objectArray.model.Phone;
+
 public class ObjectArrayRun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
