a3d4f51c9d95402d35a50a5087f9a30801bf1767
yoonyj1 <yoonyj2410@naver.com>
2022-11-08 14:49:44 +0900

    202211081449
    202211081449

--- 06_ObjectArray_Homework/src/com/hw1/model/vo/Employee.java
+++ 06_ObjectArray_Homework/src/com/hw1/model/vo/Employee.java
@@ -12,11 +12,11 @@ public class Employee {
 	private double bonusPoint; // 보너스포인트
 	private String phone; // 전화번호
 	private String address; // 주소
-	
-	
-	// 생성자부 
-	public Employee() {}
-	
+
+	// 생성자부
+	public Employee() {
+	}
+
 	public Employee(int empNo, String empName, int age, char gender, String phone, String address) {
 		this.empNo = empNo;
 		this.empName = empName;
@@ -38,68 +38,86 @@ public class Employee {
 	public int getEmpNo() {
 		return empNo;
 	}
+
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
+
 	public String getEmpName() {
 		return empName;
 	}
+
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
+
 	public String getDept() {
 		return dept;
 	}
+
 	public void setDept(String dept) {
 		this.dept = dept;
 	}
+
 	public String getJob() {
 		return job;
 	}
+
 	public void setJob(String job) {
 		this.job = job;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public char getGender() {
 		return gender;
 	}
+
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
+
 	public int getSalary() {
 		return salary;
 	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
+
 	public double getBonusPoint() {
 		return bonusPoint;
 	}
+
 	public void setBonusPoint(double bonusPoint) {
 		this.bonusPoint = bonusPoint;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
 	public String information() {
-		return empNo + ", " + empName + ", " + dept + ", " + job + ", " + age + ", " + gender + ", " + salary + ", " + bonusPoint + ", " + phone
-				+ ", " + address;
+		return empNo + ", " + empName + ", " + dept + ", " + job + ", " + age + ", " + gender + ", " + salary + ", "
+				+ bonusPoint + ", " + phone + ", " + address;
 	}
-	
-	
+
 }
