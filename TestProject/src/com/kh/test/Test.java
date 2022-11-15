fa1a8ece1b7f1faa9b7a38c611a035b4bf5f682b
yoonyj1 <yoonyj2410@naver.com>
2022-11-15 14:49:03 +0900

    202211151449
    202211151449

--- TestProject/src/com/kh/test/Test.java
+++ TestProject/src/com/kh/test/Test.java
@@ -1,7 +1,12 @@
 package com.kh.test;
 
 public class Test {
-	public void method1() {
-		System.out.println("ㅎㅇ");
-	}
+	 public static void main(String[] args) {
+		 for (int i = 2; i <= 9; i++) {
+			 System.out.println("=========== " + i + "단 ===========");
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + " X " + j + " = " + (i * j));
+			}
+		}
+	 }
 }
