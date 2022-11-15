deb148175d6c14bfc48e14236ab5974e6561c3f8
yoonyj1 <yoonyj2410@naver.com>
2022-11-15 11:50:47 +0900

    202211151150
    202211151150

---
+++ 10_Exception/src/com/kh/exception/controller/A_UncheckedException.java
@@ -0,0 +1,16 @@
+package com.kh.exception.controller;
+
+public class A_UncheckedException {
+	/*
+	 * * RuntimeException
+	 *  - 프로그램 실행 시 발생되는 예외
+	 *  
+	 * * RuntimeException의 후손들
+	 *  - IndexOutOfBoundsException: 부적절한 인덱스를 가지고 접근 시 발생되는 예외
+	 *  - NullPointerException: 참조변수가 null로 초기화 된 상황에서 null에 접근했을 때 발생되는 예외
+	 *  - ArithmeticException: 0으로 나누기 연산을 하려할 경우 발생되는 예외
+	 *  - ClassCastException: 허용할 수 없는 형변환이 진행될 때 발생되는 예외
+	 *  - NegativeArraySizeException: 배열 할당 과정에서 배열의 크기를 음수로 지정하는 경우 발생되는 예외   
+	 */
+	
+}
